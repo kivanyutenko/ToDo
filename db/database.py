8db5891d74ce965d7379e0248d393fe1ce8c307f
kivanyutenko <kivanyutenko@gmail.com>
2023-12-07 13:41:57 +0100

    test

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi_practice.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks_database.db"
  
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
