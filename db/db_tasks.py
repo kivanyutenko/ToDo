7f0a23ff45f0a7231073493eb323abf3998ec962
Selahaddin Akgul <106680786+akgulselo@users.noreply.github.com>
2024-01-02 13:21:59 +0100

    Update db_tasks.py

--- db/db_tasks.py
+++ db/db_tasks.py
@@ -1,26 +1,48 @@
-from datetime import datetime,date,time
 from sqlalchemy.orm.session import Session
-from db.models import DbFolder, DbTask, DbUser, Tag
+from db.models import DbFolder, DbTask, DbUser, DbTag 
 from schemas import TaskBase
-<<<<<<< Updated upstream
 from fastapi import  HTTPException, status
-=======
-from fastapi import  HTTPException,status
 from db.database import SessionLocal
->>>>>>> Stashed changes
+
 
 #Creating new task. By default it's status is 'New' and folder is 'Main'
-def create_task(db:Session,request:TaskBase,option,current_user):
+def create_task(db:Session,request:TaskBase,status,option,folder_id,flag,date_iso,time_iso,current_user,out_image_url, app_tag):
+    folder=db.query(DbFolder).filter(DbFolder.id==folder_id).first()
+    if not folder:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Folder with id {folder_id} not found')
+    if folder.user_id is not None and current_user.id != folder.user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You are not allowed to place task in this folder') 
+    
+    selected_tags = []
+    if app_tag:
+        # Check for existing tags
+        existing_tags = db.query(DbTag).filter(DbTag.name.in_(app_tag)).all()
+        existing_tag_names = {tag.name for tag in existing_tags}
+
+        # Create new tags if they don't exist
+        for tag_name in app_tag:
+            if tag_name not in existing_tag_names:
+                new_tag = DbTag(name=tag_name, user_id=current_user.id)
+                db.add(new_tag)
+                db.commit()
+                db.refresh(new_tag)
+                selected_tags.append(new_tag)
+            else:
+                selected_tags.extend([tag for tag in existing_tags if tag.name == tag_name])
+    
+    
     new_task=DbTask(
         title=request.title,
         description=request.description,
-        task_status='New',
+        task_status=status,
         priority=option,
-        flag=False,
-        date=datetime.now().date(),
-        time=datetime.now().time(),
-        folder_id=1,
-        user_id=current_user.id
+        date=date_iso,
+        time=time_iso,
+        flag=flag,
+        folder_id=folder_id,
+        user_id=current_user.id,
+        image_url=out_image_url,
+        app_tags=selected_tags
     )
     db.add(new_task)
     db.commit()
@@ -43,7 +65,7 @@ def get_task(db:Session,id:int,current_user):
     return task
 
 #Update  task
-def update_task(id:int,request:TaskBase,db:Session,Status:str,priority:str,flag:bool,date_iso,time_iso,folder_id:int,current_user):
+def update_task(id:int,request:TaskBase,db:Session,Status:str,priority:str,flag:bool,date_iso,time_iso,folder_id:int,current_user,image_url,app_tag):
     task=db.query(DbTask).filter(DbTask.id==id).first()
     folder=db.query(DbFolder).filter(DbFolder.id==folder_id).first()
     if not task:
@@ -52,18 +74,25 @@ def update_task(id:int,request:TaskBase,db:Session,Status:str,priority:str,flag:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Folder with id {folder_id} not found') 
     if current_user.id != task.user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You are not allowed to update this task')
-    if current_user.id !=DbFolder.user_id:
+    if folder.user_id is not None and current_user.id != folder.user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='You are not allowed to place task in this folder')
-    db.query(DbTask).filter(DbTask.id == id).update({
-        DbTask.title: request.title,
-        DbTask.description: request.description,
-        DbTask.task_status:Status,
-        DbTask.priority:priority,
-        DbTask.flag:flag,
-        DbTask.folder_id:folder_id,
-        DbTask.date:date_iso,
-        DbTask.time:time_iso
-    })
+    
+    if app_tag:
+        selected_tags = db.query(DbTag).filter(DbTag.name.in_(app_tag)).all()
+    else:
+        selected_tags = []
+    
+    task.title = request.title
+    task.description = request.description
+    task.task_status = Status
+    task.priority = priority
+    task.flag = flag
+    task.folder_id = folder_id
+    task.date = date_iso
+    task.time = time_iso
+    task.image_url = image_url
+    task.app_tags = selected_tags  # Set the app_tags relationship directly
+
     db.commit()
     return 'Success'
 
@@ -89,46 +118,36 @@ def delete_all_tasks(db: Session, current_user):
         return 'Success'
 
 # Create a new tag
-def create_tag(name: str):
-    db = SessionLocal()
-    new_tag = Tag(name=name)
-    db.add(new_tag)
+def create_app_tag(name: str,db,current_user):
+    new_app_tag = DbTag(name=name, user_id=current_user.id)
+    db.add(new_app_tag)
     db.commit()
-    db.refresh(new_tag)
-    db.close()
-    return new_tag
+    db.refresh(new_app_tag)
+    return new_app_tag
 
 # Get all tags
-def get_all_tags():
-    db = SessionLocal()
-    tags = db.query(Tag).all()
-    db.close()
-    return tags
+def get_all_app_tags(db):
+    app_tags = db.query(DbTag).all()
+    return app_tags
 
 # Get tag by ID
-def get_tag_by_id(tag_id: int):
-    db = SessionLocal()
-    tag = db.query(Tag).filter(Tag.id == tag_id).first()
-    db.close()
-    return tag
+def get_app_tag_by_id(app_tag_id: int,db):
+    app_tag = db.query(DbTag).filter(DbTag.id == app_tag_id).first()
+    return app_tag
 
 # Update tag by ID
-def update_tag(tag_id: int, new_name: str):
-    db = SessionLocal()
-    tag = db.query(Tag).filter(Tag.id == tag_id).first()
-    if tag:
-        tag.name = new_name
+def update_app_tag(app_tag_id: int, new_name: str,db):
+    app_tag = db.query(DbTag).filter(DbTag.id == app_tag_id).first()
+    if app_tag:
+        app_tag.name = new_name
         db.commit()
-        db.refresh(tag)
-    db.close()
-    return tag
+        db.refresh(app_tag)
+    return app_tag
 
 # Delete tag by ID
-def delete_tag(tag_id: int):
-    db = SessionLocal()
-    tag = db.query(Tag).filter(Tag.id == tag_id).first()
-    if tag:
-        db.delete(tag)
+def delete_app_tag(app_tag_id: int,db):
+    app_tag = db.query(DbTag).filter(DbTag.id == app_tag_id).first()
+    if app_tag:
+        db.delete(app_tag)
         db.commit()
-    db.close()
-    return tag
+    return app_tag
