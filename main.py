b70a3db130e5459ba6da87579c0468ea6eb49f4b
Selahaddin Akgul <106680786+akgulselo@users.noreply.github.com>
2024-01-02 13:15:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routers import task,folder,user
+from routers import task,folder,user, tag
 from db import models
 from db.database import engine
 from auth import authentication
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
 
@@ -12,6 +12,7 @@ app.include_router(authentication.router)
 app.include_router(task.router)
 app.include_router(folder.router)
 app.include_router(user.router)
+app.include_router(tag.router)
 
 models.Base.metadata.create_all(engine)
 
